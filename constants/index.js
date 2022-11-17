8370795b8e315d29b8b84652f8c758e46ee99436
Арслан (WL) <arslanwl@MacBook-Air-Arslan.local>
2022-11-17 22:31:35 +0500

    first commit

---
+++ constants/index.js
@@ -0,0 +1,5 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS =
+  "0x15744b041a63aD77c284EDd3B78B38FA2a2fdDBF";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
